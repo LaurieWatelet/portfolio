43e324442087d31d73183f85ce6060fc151efc35
Laurie <144000511+LaurieWatelet@users.noreply.github.com>
2023-10-09 11:37:22 +0200

    Add files via upload

---
+++ src/App.vue
@@ -0,0 +1,33 @@
+<template>
+  <div class="d-flex flex-column h-100">
+    <header>
+      <!-- Utilisation du composant Nav -->
+      <Nav /> 
+      <!-- Remplacez le code du navbar par cette balise -->
+    </header>
+
+    <!-- Begin page content -->
+    <main class="flex-shrink-0 py-5">
+      <div class="container">
+
+
+      </div>
+    </main>
+
+    <footer class="footer mt-auto py-3 bg-body-tertiary">
+
+    </footer>
+    <!-- Utilisation du composant Footer -->
+  </div>
+</template>
+
+<script setup>
+import Nav from './components/Nav.vue';
+import Footer from './components/Footer.vue';
+</script>
+
+<style>
+#app {
+  height: 100vh;
+}
+</style>
---
+++ src/components/Nav.vue
@@ -0,0 +1,41 @@
+<script setup></script>
+<template>
+  <nav class="navbar navbar-expand-md navbar-dark fixed-top bg-primary">
+    <div class="container-fluid">
+      <a class="navbar-brand" href="#">Portfolio</a>
+      <button
+        class="navbar-toggler"
+        type="button"
+        data-bs-toggle="collapse"
+        data-bs-target="#navbarCollapse"
+        aria-controls="navbarCollapse"
+        aria-expanded="false"
+        aria-label="Toggle navigation"
+      >
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="collapse navbar-collapse" id="navbarCollapse">
+        <ul class="navbar-nav me-auto mb-2 mb-md-0">
+          <li class="nav-item">
+            <a class="nav-link active" aria-current="page" href="#">Accueil</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#">Projets</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link disabled" aria-disabled="true">À propos</a>
+          </li>
+        </ul>
+        <form class="d-flex" role="search">
+          <input
+            class="form-control me-2"
+            type="search"
+            placeholder="Recherche"
+            aria-label="Search"
+          />
+          <button class="btn btn-outline-success" type="submit">Recherche</button>
+        </form>
+      </div>
+    </div>
+  </nav>
+</template>
