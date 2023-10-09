362182a77118adcf281121ef4f43ec36b4532d25
lapetitechevre <144000511+lapetitechevre@users.noreply.github.com>
2023-10-09 15:09:37 +0200

    Footer.vue

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,7 +1,22 @@
 <template>
   <footer class="footer mt-auto py-3 bg-body-tertiary">
-      <div class="container">
-        <span class="text-body-secondary">Place sticky footer content here.</span>
-      </div>
-    </footer>
+    <div class="container">
+      <p>Mon pied de page</p>
+    </div>
+  </footer>
 </template>
+
+<script>
+export default {
+  name: 'Footer'
+};
+</script>
+
+<style scoped>
+/* Styles pour le pied de page */
+footer {
+  background-color: #333;
+  color: white;
+  text-align: center;
+}
+</style>
