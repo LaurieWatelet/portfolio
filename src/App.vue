d2d3f797abde02ea6b12c21e3a069b58da4e7adb
lapetitechevre <144000511+lapetitechevre@users.noreply.github.com>
2023-10-09 12:09:17 +0200

    App.vue

--- src/App.vue
+++ src/App.vue
@@ -23,6 +23,28 @@
       Cliquez ici <!-- slot -->
     </PrimaryButton>
 
+   <Layout>
+    <template v-slot:title>
+      <h1 class="mt-5">Sticky footer with fixed navbar</h1>
+    </template>
+
+    <template v-slot:content>
+      <p class="lead">
+        Pin a footer to the bottom of the viewport in desktop browsers with this
+        custom HTML and CSS. A fixed navbar has been added with
+        <code class="small">padding-top: 60px;</code> on the
+        <code class="small">main &gt; .container</code>.
+      </p>
+      <p>
+        Back to
+        <a href="/docs/5.3/examples/sticky-footer/"
+          >the default sticky footer</a
+        >
+        minus the navbar.
+      </p>
+    </template>
+  </Layout>
+
   </div>
 </template>
 
@@ -30,6 +52,7 @@
 import PrimaryButton from './PrimaryButton.vue';
 import Nav from './components/Nav.vue';
 import Footer from './components/Footer.vue';
+import Layout from './components/Layout.vue';
 </script>
 
 <style>
